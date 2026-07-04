4cfdedc72b7c861bd62b52f6978c08cca09b46a8
Manoutoftime <priyalingeshwaran@gmail.com>
2026-07-04 21:08:28 +0530

    Add files via upload

---
+++ webfirst/script.js
@@ -0,0 +1,52 @@
+function openMenu() {
+  document.getElementById("sidebar").style.left = "0";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function closeMenu() {
+  document.getElementById("sidebar").style.left = "-250px";
+  document.getElementById("overlay").style.display = "none";
+}
+
+function main(){
+  window.open("https://www.youtube.com", "_blank");
+}
+
+function spt(){
+  window.open("https://www.spotify.com", "_blank");
+}
+
+function pt(){
+  window.open("https://www.youtube.com/watch?v=8TZMtslA3UY", "_blank");
+}
+function s(){
+window.location="sns.html";
+}
+function kt(){
+window.open("kt.html","_blank");
+}
+function toggleDark() {
+  document.body.classList.toggle("dark");
+}
+function togglePhotos() {
+  let x = document.getElementById("photos");
+  x.classList.toggle("show");
+}
+function blast() {
+    window.open("https://www.youtube.com/watch?v=62bIsvRcPv0", "_blank");
+}
+function playSong(src) {
+  let player = document.getElementById("player");
+  player.src = src;
+  player.play();
+}
+function logout(){
+  localStorage.removeItem("loggedIn");
+  window.location = "login.html"; 
+}
+function back(){
+window.location="loader.html";
+}
+function so(){
+window.open("https://spideytracker.com/","_blank");
+}
