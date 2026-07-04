3c726d15a1c24c48726de5c7fadde229f447811b
Manoutoftime <priyalingeshwaran@gmail.com>
2026-07-04 20:27:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -32,8 +32,8 @@ function togglePhotos() {
   let x = document.getElementById("photos");
   x.classList.toggle("show");
 }
-function ok(){
-windows.open("https://poki.com/en/g/blast-buddies","_blank")
+function blast(){
+windows.open("https://www.youtube.com/watch?v=62bIsvRcPv0","_blank")
 }
 function playSong(src) {
   let player = document.getElementById("player");
